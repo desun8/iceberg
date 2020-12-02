2496d18fa506aa2b0103cfbead0ac49a314cee18
Evgeniy <epchelinov@gmail.com>
2020-12-02 12:23:10 +1000

    доработки меню

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,28 +2,39 @@ import A11yDialog from 'a11y-dialog';
 import Accordion from './accordion';
 
 const initPageMenu = () => {
+  let btnOpen;
   const btnsOpen = document.querySelectorAll('.js-menu-open');
   const btnClose = document.querySelector('.js-menu-close');
   const element = document.querySelector('#page-menu');
   const dialog = new A11yDialog(element);
 
-  dialog.show();
+  const handleShow = () => {
+    btnClose.classList.add('hamburger--close');
+
+    if (btnOpen) {
+      btnOpen.classList.add('hamburger--close');
+    }
+  };
+  const handleHide = () => {
+    btnClose.classList.remove('hamburger--close');
+
+    if (btnOpen) {
+      btnOpen.classList.remove('hamburger--close');
+    }
+  };
+
+  dialog.on('show', handleShow);
+  dialog.on('hide', handleHide);
 
   btnsOpen.forEach((btn) => {
     btn.addEventListener('click', () => {
-      btn.classList.add('hamburger--close');
-      btnClose.classList.add('hamburger--close');
+      btnOpen = btn;
       dialog.show();
     });
   });
 
   btnClose.addEventListener('click', () => {
     dialog.hide();
-    btnClose.classList.remove('hamburger--close');
-
-    btnsOpen.forEach((btn) => {
-      btn.classList.remove('hamburger--close');
-    });
   });
 
   const accordionElm = element.querySelector('.accordion');
