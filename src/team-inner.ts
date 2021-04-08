07bb39a7f9253cefba8a62cb68692890cf38b0cb
Evgeniy <epchelinov@gmail.com>
2021-04-08 15:56:48 +1000

    Добавилось: — Маска на изображение сотрудника — Анимация появления кейсов

--- src/team-inner.ts
+++ src/team-inner.ts
@@ -1,4 +1,9 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import core from "./core";
+import ImageMask from "./scripts/ImageMask";
+
+gsap.registerPlugin(ScrollTrigger);
 
 core();
 
@@ -19,5 +24,30 @@ const exampleLinkHover = () => {
   });
 };
 
+const addPictureMask = () => {
+  const element = document.querySelector(".employee-picture") as HTMLDivElement;
+  if (element) {
+    new ImageMask(element);
+  }
+};
+
+const examplesEnterAnimation = () => {
+  const root = document.querySelector(".examples-list") as HTMLElement;
+  const elements = Array.from(document.querySelectorAll(".example-card")) as HTMLElement[];
+
+  gsap.from(elements, {
+    scrollTrigger: {
+      trigger: root,
+      start: "200px bottom",
+    },
+    stagger: 0.4,
+    alpha: 0,
+    y: 100,
+    duration: 0.6,
+  });
+};
+
 exampleLinkHover();
+addPictureMask();
+examplesEnterAnimation();
 
