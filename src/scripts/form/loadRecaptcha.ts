23b5e28bbfe09d79277fbd7f83862cf370167f00
Evgeniy <epchelinov@gmail.com>
2021-04-15 14:13:47 +1000

    Динамическая загрузка: — Рекапча — ImageMask (фото сотрудника)

---
+++ src/scripts/form/loadRecaptcha.ts
@@ -0,0 +1,18 @@
+// <script src="https://www.google.com/recaptcha/api.js?render=6LeDFv8ZAAAAADvO8QeneqiQyoJE0f9UOIRvt8uG"></script>
+// TODO: Переделать
+declare global {
+  interface Window {
+    grecaptcha: any;
+  }
+}
+
+export const loadRecaptcha = () => {
+  if (window.grecaptcha === undefined) {
+    const siteKey = "6LeDFv8ZAAAAADvO8QeneqiQyoJE0f9UOIRvt8uG";
+    const url = "https://www.google.com/recaptcha/api.js?render=";
+
+    const script = document.createElement("script");
+    script.src = `${url}${siteKey}`;
+    document.head.appendChild(script);
+  }
+}
