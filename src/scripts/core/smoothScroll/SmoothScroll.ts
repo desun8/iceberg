2c519574a82a28b4f42c4950614fdd56e810b343
Evgeniy <epchelinov@gmail.com>
2021-04-20 21:06:33 +1000

    Доработка плавного скролла: — Инициализируется только для не-тач-экранов. — Переделал в класс. — Небольшие стилевые правки.

---
+++ src/scripts/core/smoothScroll/SmoothScroll.ts
@@ -0,0 +1,90 @@
+import Scrollbar from "smooth-scrollbar";
+import ModalPlugin from "./ModalPlugin";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+class SmoothScroll {
+  private smoothScrollInstance?: Scrollbar;
+  private options = {
+    damping: 0.1,
+    delegateTo: document,
+    alwaysShowTracks: window.APP.isDesktop,
+  };
+
+  constructor(private element: HTMLElement | null) {
+    if (this.element) {
+      if (!window.APP.isTouchScreen) {
+        this.addPlugins();
+        this.smoothScrollInstance = Scrollbar.init(this.element, this.options);
+
+        this.setup();
+      }
+
+      this.setupAnimation();
+    } else {
+      console.warn("Не удалось инициализировать плавный скролл.");
+    }
+  }
+
+  private addPlugins() {
+    Scrollbar.use(ModalPlugin);
+  }
+
+  private addStyles() {
+    document.body.style.overflow = "hidden";
+    this.element!.style.height = "100vh";
+  }
+
+  private addToGlobal() {
+    window.APP.scrollbar = this.smoothScrollInstance;
+  }
+
+  private setupScrollTrigger() {
+    const smoothScrollInstance = this.smoothScrollInstance!;
+
+    ScrollTrigger.scrollerProxy(this.element!, {
+      scrollTop(value) {
+        if (arguments.length) {
+          if (typeof value === "number") {
+            smoothScrollInstance.scrollTop = value;
+          }
+        }
+
+        return smoothScrollInstance.scrollTop;
+      },
+    });
+
+    this.smoothScrollInstance!.addListener(ScrollTrigger.update);
+
+    ScrollTrigger.defaults({scroller: this.element!});
+  }
+
+  private setup() {
+    this.addStyles();
+    this.addToGlobal();
+    this.setupScrollTrigger();
+  }
+
+  private animationFadeIn() {
+    const elements = Array.from(document.querySelectorAll(".js-scrolltrigger-fadein"));
+
+    elements.forEach(element => {
+      gsap.from(element, {
+        y: 50,
+        alpha: 0,
+        scrollTrigger: {
+          trigger: element,
+          start: "top 70%",
+        },
+      });
+    });
+  }
+
+  private setupAnimation() {
+    this.animationFadeIn();
+  }
+}
+
+export default SmoothScroll;
