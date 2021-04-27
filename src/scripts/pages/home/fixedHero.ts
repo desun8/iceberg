1ebbcd1312f64b4d8440488469ed4d69acd2a7be
Evgeniy <epchelinov@gmail.com>
2021-04-28 02:44:06 +1000

    Поправил: — анимацию прокуртки "херо"; — карточку услуг;

--- src/scripts/pages/home/fixedHero.ts
+++ src/scripts/pages/home/fixedHero.ts
@@ -5,33 +5,40 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default () => {
   const element = document.querySelector(".block-hero") as HTMLElement;
-
-  const toggleWillChange = (shouldAdd: boolean, element: HTMLElement) => {
-    if (shouldAdd) {
-      element.style.willChange = "transform";
-    } else {
-      element.style.willChange = "auto";
-    }
-  };
-
   const isLargeHeightScreen = window.matchMedia('(min-height: 1000px)').matches;
-
   const triggerStart = isLargeHeightScreen ? "top top" : "bottom bottom"
 
-  ScrollTrigger.create({
-    trigger: element,
-    start: triggerStart,
-    pin: true,
-    pinSpacing: false,
-    pinType: "transform",
-    onEnter() {
-      toggleWillChange(true, element);
-    },
-    onEnterBack() {
-      toggleWillChange(true, element);
-    },
-    onLeave() {
-      toggleWillChange(false, element);
-    },
-  });
+  if (window.APP.isDesktop) {
+    const toggleWillChange = (shouldAdd: boolean, element: HTMLElement) => {
+      if (shouldAdd) {
+        element.style.willChange = "transform";
+      } else {
+        element.style.willChange = "auto";
+      }
+    };
+
+    ScrollTrigger.create({
+      trigger: element,
+      start: triggerStart,
+      pin: true,
+      pinSpacing: false,
+      pinType: "transform",
+      onEnter() {
+        toggleWillChange(true, element);
+      },
+      onEnterBack() {
+        toggleWillChange(true, element);
+      },
+      onLeave() {
+        toggleWillChange(false, element);
+      },
+    });
+  } else {
+    ScrollTrigger.create({
+      trigger: element,
+      start: triggerStart,
+      pin: true,
+      pinSpacing: false,
+    });
+  }
 }
