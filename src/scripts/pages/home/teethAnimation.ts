6574e8d3a7081559a6182e699c94eeaca098217f
Evgeniy <epchelinov@gmail.com>
2021-05-05 16:20:50 +1000

    "Драг" анимация для зуба. тестовый вариант.

--- src/scripts/pages/home/teethAnimation.ts
+++ src/scripts/pages/home/teethAnimation.ts
@@ -1,16 +1,21 @@
 import gsap from "gsap";
 
+const consoleStyle = (background: string) => `
+  background: ${background};
+  padding: 1em;
+  font-weight: 700`;
+
 export default () => {
   const isDesktop = window.APP.isDesktop;
 
   const container = document.querySelector("#teeth")!;
-  const teethMain = container.querySelector(".teeth__main")!;
-  const teethLeft = container.querySelector(".teeth__part--left")!;
-  const teethRight = container.querySelector(".teeth__part--right")!;
-  const teethLeftSmall = container.querySelector(".teeth__part--left-small")!;
-  const teethRightSmall = container.querySelector(".teeth__part--right-small")!;
-  const teethTopLeft = container.querySelector(".teeth__part--top-left")!;
-  const teethTopRight = container.querySelector(".teeth__part--top-right")!;
+  const teethMain = container.querySelector(".teeth__main") as HTMLElement;
+  const teethLeft = container.querySelector(".teeth__part--left") as HTMLElement;
+  const teethRight = container.querySelector(".teeth__part--right") as HTMLElement;
+  const teethLeftSmall = container.querySelector(".teeth__part--left-small") as HTMLElement;
+  const teethRightSmall = container.querySelector(".teeth__part--right-small") as HTMLElement;
+  const teethTopLeft = container.querySelector(".teeth__part--top-left") as HTMLElement;
+  const teethTopRight = container.querySelector(".teeth__part--top-right") as HTMLElement;
   const btn = container.querySelector(".teeth-btn")!;
 
   const createTeethPartsAnimation = () => {
@@ -94,71 +99,79 @@ export default () => {
     teethIconsAnimation.play();
   });
 
-  // drag
-  // const mainRect = teethMain.getBoundingClientRect();
-  //
-  // console.log(mainRect);
-  //
-  // teethRight.onmousedown = function (event) {
-  //
-  //   console.log(event);
-  //
-  //   let shiftX = event.clientX - teethRight.getBoundingClientRect().left;
-  //   let shiftY = event.clientY - teethRight.getBoundingClientRect().top;
-  //
-  //   // ball.style.position = 'absolute';
-  //   // ball.style.zIndex = 1000;
-  //   // document.body.append(ball);
-  //
-  //   // const posX = ;
-  //
-  //   // moveAt(event.pageX, event.pageY);
-  //
-  //   // переносит мяч на координаты (pageX, pageY),
-  //   // дополнительно учитывая изначальный сдвиг относительно указателя мыши
-  //   function moveAt(pageX, pageY) {
-  //     // teethRight.style.left = pageX - shiftX + "px";
-  //     // teethRight.style.top = pageY - shiftY + "px";
-  //     const posX = (mainRect.left + mainRect.width) - shiftX + "px";
-  //     const posY = (mainRect.top + mainRect.height) - shiftY + "px";
-  //     console.log(posX);
-  //     teethRight.style.transform = `translate(${posX}, ${posY})`;
-  //   }
-  //
-  //   function onMouseMove(event) {
-  //     moveAt(event.pageX, event.pageY);
-  //   }
-  //
-  //   // передвигаем мяч при событии mousemove
-  //   document.addEventListener("mousemove", onMouseMove);
-  //
-  //   // отпустить мяч, удалить ненужные обработчики
-  //   teethRight.onmouseup = function () {
-  //     document.removeEventListener("mousemove", onMouseMove);
-  //     teethRight.onmouseup = null;
-  //   };
-  //
-  // };
-  //
-  // teethRight.ondragstart = function () {
-  //   return false;
-  // };
 
-  // const move = (target: HTMLElement, clientX: number, clientY: number, pageX: number, pageY: number) => {
-  //   const {left, top} = target.getBoundingClientRect();
-  //
-  //   let shiftX = clientX - left;
-  //   let shiftY = clientY - top;
-  //
-  //   const newPosX = pageX - shiftX;
-  //   const newPosY = pageY - shiftY;
-  //
-  //   console.log(mainRect.left - left, clientY);
-  //
-  //   target.style.transform = `translate(${mainRect.left - left}px, 0)`;
-  // };
-  //
-  // teethRight!.addEventListener("mousemove", (event) => {
-  //   move(teethRight, event.clientX, event.clientY, event.pageX, event.pageY)
-  // });
+  if (!window.APP.isTouchScreen) {
+    // DRAG v2
+    const addDrag = (dragItem: HTMLElement, container = teethMain) => {
+      let shouldCheckTransform = true;
+      let active = false;
+      let currentX: number;
+      let currentY: number;
+      let initialX: number;
+      let initialY: number;
+      let xOffset = 0;
+      let yOffset = 0;
+
+      console.log("initial");
+      console.log(xOffset, yOffset);
+
+      function dragStart(this: HTMLElement, event: MouseEvent) {
+        if (shouldCheckTransform) {
+          const currentTransform = dragItem.style.transform.match(/[0-9]+/g);
+
+          if (currentTransform && currentTransform.length === 2) {
+            xOffset = parseInt(currentTransform[0]);
+            yOffset = parseInt(currentTransform[1]);
+          }
+
+          shouldCheckTransform = false;
+        }
+
+        initialX = event.clientX - xOffset;
+        initialY = event.clientY - yOffset;
+
+        if (event.target === dragItem) {
+          active = true;
+          dragItem.style.willChange = "transform";
+        }
+      }
+
+      function dragEnd() {
+        initialX = currentX;
+        initialY = currentY;
+
+        active = false;
+        dragItem.style.willChange = "";
+      }
+
+      function drag(this: HTMLElement, event: MouseEvent) {
+        if (active) {
+          event.preventDefault();
+
+          currentX = event.clientX - initialX;
+          currentY = event.clientY - initialY;
+
+          xOffset = currentX;
+          yOffset = currentY;
+
+          setTranslate(Math.floor(currentX), Math.floor(currentY), dragItem);
+        }
+      }
+
+      function setTranslate(xPos: number, yPos: number, el: HTMLElement) {
+        el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+      }
+
+      dragItem.addEventListener("mousedown", dragStart, false);
+      dragItem.addEventListener("mouseup", dragEnd, false);
+      dragItem.addEventListener("mouseleave", dragEnd, false);
+      dragItem.addEventListener("mousemove", drag, false);
+    };
+
+    const items = [teethRight, teethRightSmall, teethTopRight, teethLeft, teethLeftSmall, teethTopLeft];
+    // const items = [teethRight];
+    items.forEach((element: HTMLElement) => {
+      addDrag(element);
+    });
+  }
 }
