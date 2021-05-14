ad39af0c0ba91fbbd837edca57be8b9668f89287
Evgeniy <epchelinov@gmail.com>
2021-05-14 14:09:51 +1000

    Главная - доработал блок с видео и загрузил наиболее актуальные файлы.

--- src/scripts/declare.d.ts
+++ src/scripts/declare.d.ts
@@ -3,6 +3,7 @@ import SmoothScrollbar from "smooth-scrollbar";
 declare global {
   interface Window {
     grecaptcha: any;
+    Modernizr?: any;
     APP: {
       scrollbar: SmoothScrollbar | undefined
       isDesktop: boolean,
