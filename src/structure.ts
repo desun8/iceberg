083b87d78941fa4f8a275b3a837a45d3b9499aa9
Evgeniy <epchelinov@gmail.com>
2021-06-23 15:16:29 +1000

    Страница "структура компании" — добавил скрипт для открытия видео в фуллскрине

--- src/structure.ts
+++ src/structure.ts
@@ -1,7 +1,9 @@
 import core from "./core";
 import collapsibleText from "./scripts/pages/structure/collapsibleText";
 import maps from "./scripts/pages/structure/maps";
+import fullscreen from "./scripts/block-video/fullscreen";
 
 core();
 maps();
 collapsibleText();
+fullscreen();
